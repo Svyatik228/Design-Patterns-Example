1eaddbe450943e078acde6148182bf54daf78926
Святослав Волошко <voloshko.svyatoslav2@gmail.com>
2020-03-22 23:18:08 +0200

    Add Abstract Factory

---
+++ Abstract Factory/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Abstract_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero elf = new Hero(new ElfFactory());
+            elf.Hit();
+            elf.Run();
+
+            Hero voin = new Hero(new VoinFactory());
+            voin.Hit();
+            voin.Run();
+
+            Console.ReadLine();
+        }
+    }
+    //абстрактний класс - зброя
+    abstract class Weapon
+    {
+        public abstract void Hit();
+    }
+    // абстрактний класс - рух
+    abstract class Movement
+    {
+        public abstract void Move();
+    }
+
+    // класс арбалет
+    class Arbalet : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("We shoot from a crossbow");
+        }
+    }
+    // класс меч
+    class Sword : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Beat with a sword");
+        }
+    }
+    // рух польоту
+    class FlyMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Flying");
+        }
+    }
+    // рух - біг
+    class RunMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("We run");
+        }
+    }
+    // класс абстрактной фабрики
+    abstract class HeroFactory
+    {
+        public abstract Movement CreateMovement();
+        public abstract Weapon CreateWeapon();
+    }
+    // Фабрика створення літаючого героя з арбалетом
+    class ElfFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new FlyMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Arbalet();
+        }
+    }
+    // Фабрика створення біжучого героя з мечем
+    class VoinFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new RunMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Sword();
+        }
+    }
+    // клієнт - сам супергерой
+    class Hero
+    {
+        private Weapon weapon;
+        private Movement movement;
+        public Hero(HeroFactory factory)
+        {
+            weapon = factory.CreateWeapon();
+            movement = factory.CreateMovement();
+        }
+        public void Run()
+        {
+            movement.Move();
+        }
+        public void Hit()
+        {
+            weapon.Hit();
+        }
+    }
+}
