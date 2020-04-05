5183b6d9eee4b82a41eb4b8fe7fd4a189d626335
Святослав Волошко <voloshko.svyatoslav2@gmail.com>
2020-04-05 03:40:10 +0300

    Add new Pattern(Singleton)

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp = new Computer();
+            comp.Launch("Windowss 8.1");
+            Console.WriteLine(comp.OS.Name);
+
+            // в нас не получиться змінити ОС, так як об'єкт вже створений    
+            comp.OS = OS.getInstance("Windows 10");
+            Console.WriteLine(comp.OS.Name);
+
+            Console.ReadLine();
+        }
+    }
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS = OS.getInstance(osName);
+        }
+    }
+    class OS
+    {
+        private static OS instance;
+
+        public string Name { get; private set; }
+
+        protected OS(string name)
+        {
+            this.Name = name;
+        }
+
+        public static OS getInstance(string name)
+        {
+            if (instance == null)
+                instance = new OS(name);
+            return instance;
+        }
+    }
+}
